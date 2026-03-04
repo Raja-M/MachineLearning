51dbbd5ac4f29028235bc4f7e71a95bcf7006008
mailsujatha1@gmail.com <mailsujatha1@gmail.com>
2026-03-04 13:11:25 -0600

    new code

--- Python/pythonLearning/src/basics/argumentsinteractive.py
+++ Python/pythonLearning/src/basics/argumentsinteractive.py
@@ -7,3 +7,16 @@ print ("Hello, World!")
 # python -i for interactive
 print("Number of arguments:", len(sys.argv))
 print("Arguments:", sys.argv)
+'''
+17 / 3  # classic division returns a float
+17 // 3  # floor division discards the fractional part
+17 % 3  # the % operator returns the remainder of the division
+5 * 3 + 2  # floored quotient * divisor + remainder
+
+Python strings cannot be changed — they are immutable. Therefore, assigning to an indexed position in the string results in an error:
+
+'''
+a, b = 1, 2
+while a < 10:
+    a,b = b, a+b
+print(a)
